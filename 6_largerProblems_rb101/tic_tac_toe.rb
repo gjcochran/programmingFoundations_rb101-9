de6bde9722b96399d74e2a40fd67d874a054fa38
Garrett Cochran <garrettjcochran@gmail.com>
2022-11-16 09:58:18 -0700

    flow_chart test

---
+++ 6_largerProblems_rb101/tic_tac_toe.rb
@@ -0,0 +1,17 @@
+# description
+# tic tac toe is a 2 player game played on a 3x3 board. each player takes a turn and marks a square on the board. first player to reach 3 squares in a row, including diagonals, wins. if all 9 sqaures are marked and no player has 3 squares in a row, then the game is a tie.
+
+# sequence of game play
+# 1. display the initial empty 3x3 board
+# 2. ask the user to mark a sqaure
+# 3. computer marks a sqaure
+# 4 display the updated board state
+# 5. if winner, disply winner
+# 6. if board is full, display tie
+# 7. if neither winner nor board is full, go to #2
+# 8. play again?
+# 9. if yes, go to #1
+# 10. thanks for playing
+
+# flow chart
+# ttt_flow.drawio
